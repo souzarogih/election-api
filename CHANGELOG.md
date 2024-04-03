af839a9ea12351016c78e2dc5a46f9c660551a88
higorsouza <higor.andrade@hotmail.com>
2024-04-03 19:27:25 -0300

    Estrutura inicial do projeto

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Change Log
+All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
+
+## [0.0.1] 2024-04-03
+### Added
+- #0001 | Setup do projeto
---
+++ src/main/java/com/lifters/higorsouza/electionapi/ElectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.lifters.higorsouza.electionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectionApiApplication.class, args);
+    }
+
+}
