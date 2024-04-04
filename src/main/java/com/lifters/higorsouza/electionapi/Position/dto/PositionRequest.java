2c397fcd786a93759c73245d9a6c17d7112ae4a8
higorsouza <higor.andrade@hotmail.com>
2024-04-04 17:16:48 -0300

    Adicinando refatoração não mapeada no readme, pom.xml e arquivos de request

--- src/main/java/com/lifters/higorsouza/electionapi/Position/dto/PositionRequest.java
+++ src/main/java/com/lifters/higorsouza/electionapi/Position/dto/PositionRequest.java
@@ -1,5 +1,6 @@
 package com.lifters.higorsouza.electionapi.Position.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @Data
 public class PositionRequest {
 
+    @NotBlank(message = "The field position is mandatory")
     private String position;
+
+    @NotBlank(message = "The field electionYear is mandatory")
     private String electionYear;
 
 }
--- src/main/java/com/lifters/higorsouza/electionapi/Voter/dto/VoterRequest.java
+++ src/main/java/com/lifters/higorsouza/electionapi/Voter/dto/VoterRequest.java
@@ -1,16 +1,24 @@
 package com.lifters.higorsouza.electionapi.Voter.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class VoterRequest {
 
+    @NotBlank(message = "The field name is mandatory")
     private String name;
+
+    @CPF(message = "Invalid cpf field")
     private String cpf;
+
+    @Email(message = "Invalid email field")
     private String email;
 
 }
